31d715bc36a03a465c12bdcf6f6f1cab36789562
Nicklas Wangler <nicklas.wangler@googlemail.com>
2011-12-03 19:38:36 +0100

    Replaced PORTAGE_BINHOST by PKD_DIR
    TODOs:
    - Set PKG_DIR="/usr/portage/packages" in /etc/make.conf
      - server and client!
      - PORTAGE_BINHOST not used

--- src/client.sh
+++ src/client.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
+if [ $(whoami) != 'root' ];then
+   echo 'You must be root to run this script!'
+   exit 1	
+fi
+
 ### check network settings first
 
-USER="nick"
-HOST="192.168.2.100"
+echo "Syncing Portage Config files with 'client specific gentoo binary host': csg-binhost"
+USER="nick" 				# TODO: Rename var!
+HOST="192.168.2.100"			# TODO: Rename var!
+BIN_HOST_REMOTE_DIR="/opt/csg-binhost"
 SYNC_DIRS=(/etc/portage/ /etc/make.conf /var/lib/portage/ /etc/make.profile/ /usr/share/portage/config /etc/make.globals)
 for SYNC_DIR in ${SYNC_DIRS[@]} do
-   echo rsync -avze ssh $SYNC_DIR $USER@$HOST:/$SYNC_DIR
+   echo "Syncing " $SYNC_DIR
+   rsync -avze ssh $SYNC_DIR $USER@$HOST:$BIN_HOST_REMOTE_DIR/$USER/$SYNC_DIR
 done
 
-EMERGE_OPTS="--root /opt/csg-binhost/$USER/ --config-root /opt/csg-binhost/$USER/"
+echo "Syncing Portage Tree and buid updates"
+PORTAGE="/usr/partage"
+#PKG_DIR="/usr/portage/packages"	# TODO: set var in /etc/make.conf
+#PORTAGE_BINHOST=/usr/portage/packages	# not used, PKG_DIR is used with nfs/sshfs and 'emerge --usepkg' parameter
+EMERGE_OPTS="--root $BIN_HOST_REMOTE_DIR/$USER/ --config-root $BIN_HOST_REMOTE_DIR/$USER/"
 ssh $USER@$HOST 'emerge $EMERGE_OPTS --sync'
-ssh $USER@$HOST 'PKG_DIR="/usr/portage/packages" emerge $EMERGE_OPTS -uDN world'
+ssh $USER@$HOST 'PKG_DIR="/usr/portage/packages" emerge $EMERGE_OPTS --buildpkg --update --deep --newuse world'
 ssh $USER@$HOST 'revdep-rebuild $EMERGE_OPTS'
+echo "Mounting Portage filesystem to $PORTAGE"
+sshfs $USER@$HOST:/$BIN_HOST_REMOTE_DIR/$USER/$PORTAGE/ /$PORTAGE
+emerge --usepkg --update --deep --newuse world
 
-sshfs $USER@$HOST:/usr/portage/ /usr/portage
-PORTAGE_BINHOST=/usr/portage/packages emerge -uDN world # +binpkd zusatz
+echo "Umounting Portage"
+umount /$Portage
+
+exit 0
